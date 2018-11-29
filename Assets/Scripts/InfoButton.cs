b95515e6f95b7c0dca272eef3278df766e3d2ce2
rprakitpong <rprakitpong@gmail.com>
2018-11-29 11:38:38 -0800

    initial commit

---
+++ Assets/Scripts/InfoButton.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InfoButton : MonoBehaviour {
+
+    public GameObject Info;
+    bool ShowOrHide;
+
+	// Use this for initialization
+	void Start () {
+        ShowOrHide = true;
+        ShowHide();
+
+        this.gameObject.GetComponent<Button>().onClick.AddListener(ShowHide);
+	}
+
+    private void ShowHide()
+    {
+        if (ShowOrHide)
+        {
+            ShowOrHide = false;
+        } else
+        {
+            ShowOrHide = true;
+        }
+
+        Info.GetComponent<Renderer>().enabled = ShowOrHide;
+    }
+}
